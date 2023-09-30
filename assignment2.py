249311af82107c914b5f599ff5aa0208f7434a0f
elviraml0712 <122254382+elviraml0712@users.noreply.github.com>
2023-09-30 11:53:36 +0630

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,47 @@
+print("Exercise 2")
+
+famMember = input("Enter a family member of yours :")
+place = input("Enter where they live :")
+print("My", famMember, "lives in", place, "with someone.")
+
+print("//////////////////////////////////////")
+
+print("Exercise 3")
+
+name_list = ["Mark", "Jason", "Nichole", "Stephaine"]
+print(name_list[0], name_list[2])
+
+print("//////////////////////////////////////")
+
+print("Exercise 4")
+
+name = input("Please Enter Your Name :")
+age = input("Please Enter Your Age :")
+print("Hello,", name, "You are", age, "years old now. You can access to this program!")
+
+print("//////////////////////////////////////")
+
+print("Exericse 5")
+
+print("Hello, Everyone! I'm a beginner coder. Let's learn Python Programming language together. Let me know if you need more exercises!")
+
+print("//////////////////////////////////////")
+
+print("Exercise 9")
+
+kg = input("Please enter your weight in kg: ")
+m = input("Please Enter your height in m:")
+
+print("//////////////////////////////////////")
+
+print("Exercise 10")
+
+a = 5
+b = 8
+result = (a + b) * (a + b)
+print("(5 + 8) * (5 + 8) :", result)
+
+print("//////////////////////////////////////")
+
+print("Exercise 11")
+
